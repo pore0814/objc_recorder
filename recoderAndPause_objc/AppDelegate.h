3fee79dcc4cef3552509c544f18905a4ce5f926f
002563 <AnnieWang@transglobe.com.tw>
2018-10-02 10:22:19 +0800

    Initial Commit

---
+++ recoderAndPause_objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  recoderAndPause_objc
+//
+//  Created by 002563 on 2018/10/2.
+//  Copyright © 2018年 002563. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ recoderAndPause_objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  recoderAndPause_objc
+//
+//  Created by 002563 on 2018/10/2.
+//  Copyright © 2018年 002563. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
